43c3be7ad9a71fc73781959c35fa53e3a695f8d8
unknown <mohammed_ahmed4949@yahoo.com>
2023-01-16 01:19:55 +0200

    Created project list class and some basic methods

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,54 @@
 // Code goes here!
+
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLElement;
+  contentElement: HTMLElement;
+  titleInput: HTMLInputElement;
+  descInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
+
+  constructor() {
+    this.templateEl = <HTMLTemplateElement>(
+      document.getElementById("project-input")
+    );
+
+    this.hostEl = <HTMLElement>document.getElementById("app");
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+
+    this.contentElement = <HTMLElement>importedNode.firstElementChild;
+    this.contentElement.id = "user-input";
+
+    this.titleInput = <HTMLInputElement>(
+      this.contentElement.querySelector("#title")
+    );
+    this.descInput = <HTMLInputElement>(
+      this.contentElement.querySelector("#description")
+    );
+    this.peopleInput = <HTMLInputElement>(
+      this.contentElement.querySelector("#people")
+    );
+
+    this.configure();
+    this.attachElemnt();
+  }
+
+  private onSubmitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInput.value);
+  }
+
+  private configure() {
+    this.contentElement.addEventListener(
+      "submit",
+      this.onSubmitHandler.bind(this)
+    );
+  }
+
+  private attachElemnt() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.contentElement);
+  }
+}
+
+const project = new ProjectInput();
